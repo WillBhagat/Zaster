43333af312a81c84454b3f5263f11af60738eedb
WillBhagat <39274234+WillBhagat@users.noreply.github.com>
2019-10-04 17:10:14 +0100

    Initial commit

--- app/src/main/java/com/example/zaster/repositories/NetworkTask.kt
+++ app/src/main/java/com/example/zaster/repositories/NetworkTask.kt
@@ -23,26 +23,25 @@ class NetworkTask {
     {
         //the requirement was for the URL to be from application configuration but I have not heard of this before and could not find this in my research
         val request = Request.Builder()
-                .url("www.google.com")
+                .url("https://google.com/")
 
         val mediaType = "application/x-www-form-urlencoded".toMediaType()
-        val body = Resources.getSystem().getString(R.string.login_info, password, email).toRequestBody(mediaType)
+        //tried to get resource from strings folder however have not found a way to implement this properly
+        val body = ("email=" + email + "&password=" + password).toRequestBody(mediaType)
         request.post(body)
         val client = OkHttpClient.Builder().build()
-        //return this as ResponseCode
         val execute = client.newCall(request.build()).execute()
-
-        return handleResponseCode(execute.code, execute.body)
+        return handleResponseCode(execute.code, execute.body?.string())
 
     }
 
     //using when as an expression body to simplify the code
     //this causes duplicated code as values cannot be instantiated in the body of the method
-    private fun handleResponseCode(code : Int, body : ResponseBody?) : Response = when(code) {
+    private fun handleResponseCode(code : Int, body : String?) : Response = when(code) {
         200 ->
         {
             val gson = GsonBuilder().create()
-            val message = gson.fromJson(body?.string(), Valid::class.java)
+            val message = gson.fromJson(body, Valid::class.java)
             Response(message.token, ResponseCode.VALID)
         }
         401 ->
@@ -52,7 +51,7 @@ class NetworkTask {
         500 ->
         {
             val gson = GsonBuilder().create()
-            val message = gson.fromJson(body?.string(), Internal::class.java)
+            val message = gson.fromJson(body, Internal::class.java)
             Response(message.error, ResponseCode.INTERNAL)
         }
         else -> Response("Unexpected error code", ResponseCode.INTERNAL)
